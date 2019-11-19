cb07708eb75184b18d3e6b7062eaccebfb142002
Lukas Janek <lukas.janek1111@gmail.com>
2019-11-19 11:56:18 +0100

    oprava foru v nku

--- zadanie/zadanie.cpp
+++ zadanie/zadanie.cpp
@@ -1,3 +1,67 @@
-int n,k
-ada
-dada
+#include <stdio.h>
+#define max 999
+void n();
+//int v();
+//int u();
+//int s();
+//int d();
+//int h();
+//int c();
+//int k();
+
+int main() //telo
+{
+	int znak;
+	printf("Stlac znak pre:\n n- nacitanie sifrovanej spravy\n v- vypis sifrovanej spravy\n u- uprava spravy\n s- vypis sifry\n d- vypis vsetkych slov x dlzky\n h histogram sifry\n c- cezarova sifra\n k- ukoncit program\n");
+	znak = getchar();
+	
+	switch (znak)
+	{
+		case 'n': n();
+			break;
+		/*case 'v': v();
+			break;
+		case 'u': u();
+			break;
+		case 's': s();
+			break;
+		case 'd': d();
+			break;
+		case 'h': h();
+			break;
+		case 'c': c();
+			break;
+		case 'k': k();
+			break;*/ 
+	}
+	return 0;
+}
+
+void n()
+{   
+	int povodnytext[max],pismeno,pocetznakov;
+	FILE *fr;
+	if ((fr = fopen("sifra.txt","r"))==NULL)
+	{
+		printf("Spravu sa nepodarilo nacitat\n");
+	}
+	for (pocetznakov=0; pocetznakov <=max; pocetznakov++)
+	{
+		pismeno = getc(fr);
+		povodnytext[pocetznakov]=pismeno;
+	}
+	fclose(fr);
+	if (fclose(fr)==EOF)
+	{
+		printf("Subor sa nepodarilo zatvorit");
+	}
+}
+
+
+
+
+
+
+
+
+
